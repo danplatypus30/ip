981f0b3147b357ef04a39ef35f52e3ec92ee2f46
danplatypus30 <daniel.gohch@hotmail.com>
2025-01-24 02:04:44 +0800

    changed duke to miku, base done

---
+++ src/main/java/Miku.java
@@ -0,0 +1,16 @@
+public class Miku {
+    public static void main(String[] args) {
+        String logo = " __  __ _ _          \n"
+                + "|  \\/  (_) | ___   _ \n"
+                + "| |\\/| | | |/ / | | |\n"
+                + "| |  | | |   <| |_| |\n"
+                + "|_|  |_|_|_|\\_\\\\__,_|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Miku!!");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
