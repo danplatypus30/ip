0afd63cc1ac56a893d3236ede0641c563aeb52ba
danplatypus30 <daniel.gohch@hotmail.com>
2025-01-24 02:38:31 +0800

    added list features, all input is added to list, list is flushed after listed

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -1,25 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Miku {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
         String logo = " __  __ _ _          \n"
                 + "|  \\/  (_) | ___   _ \n"
                 + "| |\\/| | | |/ / | | |\n"
                 + "| |  | | |   <| |_| |\n"
                 + "|_|  |_|_|_|\\_\\\\__,_|\n";
         String line = "____________________________________________________________";
-        System.out.println(line);
-        System.out.println("Hello! I'm\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        String user_in = "";
+        System.out.println(line + "\nHello! I'm\n" + logo);
+        System.out.println("What can I do for you?\n" + line);
+
+
+        String user_in;
         do { 
             user_in = sc.nextLine();
-            if (user_in.equals("bye") == false) {
-                System.out.println(line + "\n" + user_in + "\n" + line);
+            if (user_in.equals("list")) {
+                // print list
+                System.out.println(line);
+                for (int i = 0; i < list.size(); i ++) {
+                    System.out.println((i+1) + ". " + list.get(i));
+                }      
+                System.out.println(line);
+                list.clear();
+            } else if (user_in.equals("bye") == false) {
+                System.out.println(line + "\nadded: " + user_in + "\n" + line);
+                list.add(user_in);
             }
         } while (user_in.equals("bye") == false);
+
+
         System.out.println(line + "\nBye. Hope to see you again soon!\n" + line);
         sc.close();
     }
