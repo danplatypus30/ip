0ebdbcbccdeeeae082bcbb612674e6cf07d0e885
danplatypus30 <daniel.gohch@hotmail.com>
2025-02-11 04:43:49 +0800

    Update Launcher.java

--- src/main/java/duke/Launcher.java
+++ src/main/java/duke/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 /**
  * A launcher class to workaround classpath issues.
+ * This is the entry point to the application.
  */
 public class Launcher {
     public static void main(String[] args) {
